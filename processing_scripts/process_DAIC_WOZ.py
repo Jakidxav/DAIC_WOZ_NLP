e28bdefd0010aae43c31b74a80e53b9e2231217a
Jakidxav <joshua.driscol@gmail.com>
2019-04-07 16:46:50 -0700

    updating CSV to TXT file for error checking and automation

--- processing_scripts/process_DAIC_WOZ.py
+++ processing_scripts/process_DAIC_WOZ.py
@@ -17,6 +17,18 @@ import numpy as np
 import os
 
 this_dir = './'
+part_dir = './Part/raw/'
+full_dir = './Full/raw/'
+
+#make sure directories exist; if not, create them
+if os.path.isdir(part_dir) == False:
+	os.makedirs(part_dir)
+
+if os.path.isdir(full_dir) == False:
+	os.makedirs(full_dir)
+
+#boolean for rerouting output: participant = True for participant only, participant = False for full transcript
+participant = False
 
 for subdir, dirs, files in os.walk(this_dir):
 	for file in files:
@@ -24,18 +36,21 @@ for subdir, dirs, files in os.walk(this_dir):
 			#print(file)
 			#NOTE: the filename should be constructed as 'num_TRANSCRIPT.csv'
 			#save output file as the original name, but as a text file
-			out_file_part = file.replace('.csv', '_part_raw.txt')         #participant only
-			out_file_full = file.replace('.csv', '_full_raw.txt')   #full transcript
+			out_file_part = file.replace('.csv', '_part_raw.txt')      #participant only
+			out_file_full = file.replace('.csv', '_full_raw.txt')      #full transcript
 
 			#read in the CSV file as a dataframe, tab delimited
 			df = pd.read_csv(file, delimiter='\t')
 
-			#only select the rows that correspond to the participant's speech
-			#or comment out the line below for the full transcript
-			#df = df[df.speaker=='Participant']
+			#only select the rows that correspond to the participant's speech or select full transcript
+			if participant == True:
+				df = df[df.speaker=='Participant']
 
 			#discard all columns but the participant's speech, save as .txt file
-			np.savetxt(out_file_full, df.value.values, fmt=['%s'])
+			if participant == True:
+				np.savetxt(part_dir + out_file_part, df.value.values, fmt=['%s'])
+			else:
+				np.savetxt(full_dir + out_file_full, df.value.values, fmt=['%s'])
 
 
 
